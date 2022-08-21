88414765da08c555ca45b94645732a55b9fe22c7
shaggy <someaim1@gmail.com>
2022-08-21 08:12:53 +0545

    Added New Files

---
+++ bisectionMethod.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define e 0.001
+#define f(x) x*x*x-4*x-9
+int main()
+{
+	float x0,x1,x2;
+	float f0,f1,f2;
+	int i=0;
+	system("cls");
+	printf("enter the values of two initial guess:\n");
+	scanf("%f%f",&x0,&x1);
+	do
+	{
+		f0=f(x0);
+		f1=f(x1);
+		x2=(x0+x1)/2;
+		f2=f(x2);
+		if(f0*f2<0)
+		{
+			x1=x2;
+		}
+		else
+		{
+			x0=x2;
+		}
+		i++;
+		printf("no of iterations=%d",i);
+		printf("\n root is %f",x2);
+	}
+	while(fabs(f2)>e);
+	getch();
+}
